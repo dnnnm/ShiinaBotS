1b248f72a6aa5066ced6f02ceb004bd51559ee36
dnm <74846398+dnnnm@users.noreply.github.com>
2023-05-08 12:08:11 +0700

    Add files via upload

--- plugins/z-dallE.js
+++ plugins/z-dallE.js
@@ -1,19 +1,35 @@
-import fetch from 'node-fetch'
-
-let handler = async (m, { conn, command, args, usedPrefix }) => {
-
-if (!args[0]) return conn.reply(m.chat, `*[ ! ] Membuat gambar dari AI.\nContoh:\n${usedPrefix}${command} Wooden house on snow mountain`, m)
-
-let ss = await (await fetch(`https://api.lolhuman.xyz/api/dall-e?apikey=SGWN&text=${args[0]}`)).buffer()
-
-conn.sendFile(m.chat, ss, '', '', m)}
+import { Configuration, OpenAIApi } from "openai";
+let handler = async (m, { conn, text }) => {
+  if (!text) throw "[!] Masukkan teks.";
+  const configuration = new Configuration({
+    apiKey: "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" || process.env.openai,
+  });
+  const openai = new OpenAIApi(configuration);
+  let error = 0;
+  function ai() {
+    openai
+      .createImage({
+        prompt: text,
+        n: 1,
+        //    size: "1024x1024"
+      })
+      .then((response) => {
+        return conn.sendFile(m.chat, response.data.data[0].url, "", ``, m);
+      })
+      .catch((e) => {
+        console.log(e);
+        if (error > 4) {
+          throw "Sebentar ada kesalahan pada bot!";
+        }
+        error++;
+        return ai();
+      });
+  }
+  return ai();
+};
 
 handler.help = ['dall']
-
 handler.tags = ['']
-
 handler.alias = ['dall', 'dalle', 'dall-e']
-
 handler.command = /^dall(-)?e?$/i
-
 export default handler
