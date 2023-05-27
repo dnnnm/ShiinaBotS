ba912acf8893915cd52018af12fc0dafbd405a8e
dnm <74846398+dnnnm@users.noreply.github.com>
2023-05-27 23:01:13 +0700

    Add files via upload

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -2,17 +2,22 @@ import axios from 'axios'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `Input *URL*`
     if (!args[0].match(/https:\/\/www.instagram.com\/(p|reel|tv)/gi)) throw `*Link salah! Perintah ini untuk mengunduh postingan ig/reel/tv, bukan untuk highlight/story!*\n\nContoh:\n${usedPrefix + command} https://www.instagram.com/p/BmjK1KOD_UG/?utm_source=ig_web_copy_link`
-    let api = await (await axios.get(`https://sh.xznsenpai.xyz/api/igdl?url=`+args[0])).data
-    let results = api.media[0] || api.media
+    let api = await axios.get(`https://sh.xznsenpai.xyz/api/igdl?url=`+args[0])
+    let results = api.data.media
+    let wm = `
+    ${api.data.caption}
+    
+    *${global.wm}*
+    `
     await m.reply('Sedang diproses...')
-  for (let e of api.media)
+    for (let e of api.data.media)
     await conn.sendFile(m.chat, e, '', wm, m)
 }
+
 handler.help = ['instagram'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 handler.alias = ['ig', 'igdl', 'instagram', 'instagramdl']
 handler.level = 3
 handler.command = /^(ig(dl)?|instagram(dl)?)$/i
-export default handler
 
-//https://github.com/Chandra-XD/ItsukaChan/tree/0672bf93778ce7aa0b9d14d5d9294c88bc7c3bbd
+export default handler
--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,36 +1,25 @@
-/*import { tiktokdl, tiktokdlv2, tiktokdlv3 } from '@bochilteam/scraper'
-import fetch from 'node-fetch'
-
+import axios from 'axios'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-
-  const { author: { nickname }, video, description } = await tiktokdl(args[0])
-        .catch(async _ => await tiktokdlv2(args[0]))
-        .catch(async _ => await tiktokdlv3(args[0]))
-    const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd
-    if (!url) throw 'Can\'t download video!'
-    conn.sendHydrated(m.chat, `${htki} ᴛɪᴋᴛᴏᴋ ᴡᴍ ${htka}`, `➔ ɴɪᴄᴋɴᴀᴍᴇ ${nickname}${description ? `\n➔ ᴅᴇsᴄʀɪᴘᴛɪᴏɴ:\n${description}` : ''}`, await (await fetch(url)).buffer(),
-        url, '🌎 s ᴏ ᴜ ʀ ᴄ ᴇ', null, null, [
-        ['ᴅᴏɴᴀꜱɪ', `.donasi`],
-        ['ᴀᴜᴅɪᴏ', `.tiktokaudio ${args}`],
-        [null, null]], m)
-}*/
-
-import { tiktokdl, tiktokdlv2 } from '@bochilteam/scraper'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-    const { author: { nickname }, video, description } = await tiktokdl(args[0]).catch(async _ => await tiktokdlv2(args[0]))
-    const url = video.no_watermark_raw || video.no_watermark || video.no_watermark_hd || video.with_watermark 
-    if (!url) throw 'Can\'t download video!'
-    conn.sendFile(m.chat, url, 'tiktok.mp4', `
-🧏 *Nickname:* ${nickname}${description ? `🖹 *Description:* ${description}` : ''}
-`.trim(), m)
-      }
+    if (!args[0]) throw `Input *URL*`
+        let p = await axios.get(`https://sh.xznsenpai.xyz/api/tiktok?url=`+args[0])
+        let o = p.data.data.hdplay
+        let i = `
+        ⎚ Title : ${p.data.data.title}
+        ⎚ Duration : ${p.data.data.duration}
+        ⎚ Play : ${p.data.data.play_count}
+        ⎚ Comment : ${p.data.data.comment_count}
+        ⎚ Share : ${p.data.data.share_count}
+        ⎚ Download : ${p.data.data.download_count}
+        ⎚ Author : ${p.data.data.author.nickname}
+        
+        *${global.wm}*
+        `
+        await m.reply('Sedang diproses...')
+        await conn.sendFile(m.chat, o, '', i, m)
+        }
 
 handler.help = ['tiktok', 'tiktok', 'tiktokdl'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 handler.command = /^(tik(tok)?(tok)?(dl)?)$/i
 
 export default handler
-
-//https://github.com/Conejitobrr/bot/blob/master/plugins/downloader-tiktok.js
