871da8aa0cce697245d401861e01de35bf5f3c5b
dnm <74846398+dnnnm@users.noreply.github.com>
2023-05-28 17:57:37 +0700

    Add files via upload

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -4,11 +4,9 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0].match(/https:\/\/www.instagram.com\/(p|reel|tv)/gi)) throw `*Link salah! Perintah ini untuk mengunduh postingan ig/reel/tv, bukan untuk highlight/story!*\n\nContoh:\n${usedPrefix + command} https://www.instagram.com/p/BmjK1KOD_UG/?utm_source=ig_web_copy_link`
     let api = await axios.get(`https://sh.xznsenpai.xyz/api/igdl?url=`+args[0])
     let results = api.data.media
-    let wm = `
-    ${api.data.caption}
+    let wm = `${api.data.caption}
     
-    *${global.wm}*
-    `
+    *${global.wm}*`
     await m.reply('Sedang diproses...')
     for (let e of api.data.media)
     await conn.sendFile(m.chat, e, '', wm, m)
--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -2,18 +2,10 @@ import axios from 'axios'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `Input *URL*`
         let p = await axios.get(`https://sh.xznsenpai.xyz/api/tiktok?url=`+args[0])
-        let o = p.data.data.hdplay
-        let i = `
-        ⎚ Title : ${p.data.data.title}
-        ⎚ Duration : ${p.data.data.duration}
-        ⎚ Play : ${p.data.data.play_count}
-        ⎚ Comment : ${p.data.data.comment_count}
-        ⎚ Share : ${p.data.data.share_count}
-        ⎚ Download : ${p.data.data.download_count}
-        ⎚ Author : ${p.data.data.author.nickname}
+        let o = p.data.data.play
+        let i = `${p.data.data.title}
         
-        *${global.wm}*
-        `
+        *${global.wm}*`
         await m.reply('Sedang diproses...')
         await conn.sendFile(m.chat, o, '', i, m)
         }
