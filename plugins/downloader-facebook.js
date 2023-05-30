c09040c7567dab417dd54279c77656b5016ef568
dnm <74846398+dnnnm@users.noreply.github.com>
2023-05-30 20:05:47 +0700

    update plugins/downloader-facebook.js

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -1,1 +1,15 @@
-[object ArrayBuffer]
+import fetch from 'node-fetch'
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `Input *URL*`
+    let p = await fetch(`https://xznsenpai.xyz/api/download?url=`+args[0])
+    let v = await p.json()
+    let o = v.url[0].url
+    await m.reply('Sedang diproses...')
+    await conn.sendFile(m.chat, o, '', wm, m)
+        }
+        
+handler.help = ['facebook'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^((facebook|fb)(downloder|dl)?)$/i
+handler.level = 3
+export default handler
