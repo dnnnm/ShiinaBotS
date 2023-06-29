ec5594416058c579a3e709b154022ff742b96149
dnm <74846398+dnnnm@users.noreply.github.com>
2023-06-29 19:11:32 +0700

    Update tool-readviewonce.js

--- plugins/tool-readviewonce.js
+++ plugins/tool-readviewonce.js
@@ -10,6 +10,6 @@ let handler = async (m, { conn }) => {
 
 handler.help = ['readviewonce']
 handler.tags = ['tools']
-handler.command = /^readviewonce/i
+handler.command = /^r/i
 
 export default handler
